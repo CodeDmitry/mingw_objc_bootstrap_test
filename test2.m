41e24deb285e8c00c5edb4d835e3511aef2408ae
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2023-11-20 07:35:22 -0500

    Update test2.m

--- test2.m
+++ test2.m
@@ -1,11 +1,17 @@
+// Last modified: November 20, 2023, Monday, 7:34AM EST
+// Demonstrates instantiation and subclassing of RootObject, 
+// as well as catching messages and forwarding them to the
+// superclass.
 #include "RootObject.h"
 #include <stdio.h>
 
-// proof of concept to demonstrate instantiation/subclassing of RootObject
+// The interface.
 @interface Greeter : RootObject {}
 - (void) greet;
 - (id) greetWithMessage:(const char *)theMessage;
 @end
+
+// The implementation.
 @implementation Greeter 
 - (void) greet {
     puts("[Greeter:greet] Hello, World!");
@@ -14,6 +20,11 @@
     printf("[Greeter:greetWithMessage] %s\n", theMessage);
     return self;
 }
+// Trap the release message before forwarding it over to our superclass(RootObject).
+- (void) release {
+    puts("[Greeter:release] Goodbye!");
+    [super release];
+}
 @end
 
 int main(int argc, char **argv)
@@ -21,6 +32,7 @@ int main(int argc, char **argv)
     id greeter = [[Greeter alloc] init];
     [greeter greet];
     [greeter greetWithMessage: "Hello there!"];
+    [greeter release];
 
     return 0;
 }
